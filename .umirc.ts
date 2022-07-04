03ade8246faa3dcaeacf1416ef5b60f8e3e9e092
wuwenbang <wuwenbang@foxmail.com>
2022-07-04 23:28:30 +0800

    ci: 添加CI配置文件

--- .umirc.ts
+++ .umirc.ts
@@ -7,5 +7,10 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  devServer: {
+    port: 1998, // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? '/suda/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/suda/' : '/',
   // more config: https://d.umijs.org/config
 });
