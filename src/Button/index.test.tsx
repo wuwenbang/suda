37db1c4c1974a3197084ed56cbbe646b02b44c97
wwb吴文邦 <wenbangwu@trip.com>
2022-07-06 19:45:41 +0800

    feat: 添加Button组件

--- src/Button/index.test.tsx
+++ src/Button/index.test.tsx
@@ -1,13 +1,12 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Foo from './index';
+import Button from './index';
 
-describe('<Foo />', () => {
-  it('render Foo with dumi', () => {
-    const msg = 'dumi';
-
-    render(<Foo title={msg} />);
+describe('<Button />', () => {
+  it('render Button', () => {
+    const msg = 'Button';
+    render(<Button>{msg}</Button>);
     expect(screen.queryByText(msg)).toBeInTheDocument();
   });
 });
