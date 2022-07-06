85adc8caf16237d6e06501f689edef49da829e56
wuwenbang <wuwenbang@foxmail.com>
2022-07-07 07:35:15 +0800

    build: 更改build配置

--- .umirc.ts
+++ .umirc.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'suda',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: '/images/icon-soda-16.png',
+  logo: '/images/icon-soda-80.png',
   outputPath: 'docs-dist',
   mode: 'site',
   devServer: {
