81bd32c75f217b2fb81a60f1e1c2e02739606a72
wuwenbang <wenbangwu@trip.com>
2022-07-07 07:35:15 +0800

    build: 更改build配置

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,10 +1,12 @@
 import React, { HTMLAttributes } from 'react';
+import classNames from 'classnames';
+import './index.less';
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   type?: 'primary' | 'default';
   disabled?: boolean;
 }
 
-export default function Button({ type, disabled, ...props }: ButtonProps) {
-  return <button {...props}></button>;
+export default function Button({ type, disabled, className, ...props }: ButtonProps) {
+  return <button {...props} className={classNames(className, 'suda-button')}></button>;
 }
