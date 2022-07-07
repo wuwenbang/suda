1af51474838e416b85fb561a1f40d2f8dfa75b0c
wwb吴文邦 <wenbangwu@trip.com>
2022-07-07 20:07:06 +0800

    feat: 完善Button组件

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -3,10 +3,32 @@ import classNames from 'classnames';
 import './index.less';
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  /**
+   * @description 按钮类型
+   * @default "default"
+   */
   type?: 'primary' | 'default';
+  /**
+   * @description 禁用
+   * @default false
+   */
   disabled?: boolean;
+  className?: string;
 }
 
-export default function Button({ type, disabled, className, ...props }: ButtonProps) {
-  return <button {...props} className={classNames(className, 'suda-button')}></button>;
+export default function Button({ type, disabled, className, children, ...props }: ButtonProps) {
+  // const classNameList = [className];
+  return (
+    <button
+      {...props}
+      className={classNames(
+        className,
+        'suda-button',
+        type === 'primary' ? 'suda-button-primary' : null,
+      )}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 }
