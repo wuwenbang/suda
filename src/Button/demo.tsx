931756b74edf7666c4712991201b378df22c9a5f
wuwenbang <wuwenbang@foxmail.com>
2022-07-07 20:07:06 +0800

    feat: 完善Button组件

---
+++ src/Button/demo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'suda';
+
+const Demo = () => {
+  return (
+    <div style={{ display: 'flex', gap: 12 }}>
+      <Button>Default Button </Button>
+      <Button type="primary">Primary Button </Button>
+      <Button disabled> Disable Button </Button>
+    </div>
+  );
+};
+
+export default Demo;
--- src/Button/index.md
+++ src/Button/index.md
@@ -8,11 +8,6 @@ nav:
 
 Demo:
 
-```tsx
-import React from 'react';
-import { Button } from 'suda';
+<code src="./demo.tsx"></code>
 
-export default () => <Button> Button </Button>;
-```
-
-More skills for writing demo: https://d.umijs.org/guide/basic#write-component-demo
+<API></API>
