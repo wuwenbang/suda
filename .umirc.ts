703ea5948388a2ac33c447d3bff3fbcb1fdba992
wuwenbang <wuwenbang@foxmail.com>
2022-07-07 20:07:06 +0800

    feat: 完善Button组件

--- .umirc.ts
+++ .umirc.ts
@@ -2,12 +2,23 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'suda',
-  favicon: '/images/icon-soda-16.png',
-  logo: '/images/icon-soda-80.png',
+  favicon: 'favicon.ico',
+  logo: 'logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
   devServer: {
     port: 1998, // 自定义端口号
+  },
+  apiParser: {
+    // 自定义属性过滤配置，也可以是一个函数，用法参考：https://github.com/styleguidist/react-docgen-typescript/#propfilter
+    propFilter: {
+      // 是否忽略从 node_modules 继承的属性，默认值为 false
+      skipNodeModules: true,
+      // 需要忽略的属性名列表，默认为空数组
+      // skipPropsWithName: ['title'],
+      // 是否忽略没有文档说明的属性，默认值为 false
+      // skipPropsWithoutDoc: true,
+    },
   },
   base: process.env.NODE_ENV === 'production' ? '/suda/' : '/',
   publicPath: process.env.NODE_ENV === 'production' ? '/suda/' : '/',
