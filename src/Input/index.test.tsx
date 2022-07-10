ed188bb7481032ab41b4a90eb3e974a96b80ee1b
wuwenbang <wuwenbang@foxmail.com>
2022-07-10 17:44:21 +0800

    test: 完成Button和Input的测试

--- src/Input/index.test.tsx
+++ src/Input/index.test.tsx
@@ -6,7 +6,7 @@ import Input from './index';
 describe('<Input />', () => {
   it('render Input', () => {
     const msg = 'InputTest';
-    render(<Input value={msg} />);
-    expect(screen.queryByText(msg)).toBeInTheDocument();
+    render(<Input value={msg} onChange={(value) => console.log(value)} />);
+    expect(screen.getByDisplayValue(msg)).toBeInTheDocument();
   });
 });
